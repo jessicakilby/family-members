f9ed8bf4b3f2fa80a6355517e3549d1a7fbbb291
jessica kilby <jessicakilby@gmail.com>
2016-11-13 13:28:39 -0600

    it talks to firebase, but not right

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,10 +5,29 @@ let uid = "";
 
 $(document).ready(function(){
 
+	function familyDOM(){
+		FamilyMembers.addFamily(apiKeys, uid).then(function(){
+			console.log("familyDOM");
+		});
+	}
+
 	FamilyMembers.firebaseCredentials().then(function(keys){
 		console.log("keys", keys);
 		apiKeys = keys;
 		firebase.initializeApp(apiKeys);
+	});
+
+	$("#addMember").on("click", function(){
+		let newMember = {
+			"name": $("#name").val(),
+			"age": $("#age").val(),
+			"gender": $("#gender").val(),
+			"skills": $("#skills").val(),
+			"uid": uid
+		};
+		FamilyMembers.addFamily(apiKeys, newMember).then(function(){
+			familyDOM();
+		});
 	});
 	
 });
